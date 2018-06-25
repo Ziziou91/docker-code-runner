41ff9b317453a80982190c704523b5e1742e67b7
Ziziou91 <joshgray91@gmail.com>
2018-06-25 14:10:44 +0100

    implement fully working test for q1

--- tests/1.js
+++ tests/1.js
@@ -1,27 +1,3 @@
 module.exports = {
-    test: "describe('hello world', function() {it('returns the square of 0', function(){Test.assertSimilar(result(0),ctrl(0));}); });"   
+    test: "describe('hello world',()=>{it('returns the square of 0', ()=>{Test.assertSimilar(result(0),ctrl(0));});it ('returns the square of 2',()=>{Test.assertEquals(result(2), ctrl(2));});it ('returns the square of 9',()=>{Test.assertEquals(result(9), ctrl(9));});it('returns the square of random number',()=>{let randNum = Math.round(Math.random() * 100); Test.assertEquals(result(randNum), ctrl(randNum));});});"   
 }
-
-// "describe('Square a Number', function(){it ('returns the square of 0', function(){Test.assertEquals(a, 0;});});"   
-
-//describe('hello world', function() {it('returns the square of 0', function(){Test.assertSimilar(a,1);}); });
-// module.exports = {
-//     test : function (code, control) {
-//         Test.describe("Square a Number", function(){
-//             it ('returns the square of 0', function(){
-//                 console.log('returns the square of 0')
-//                 Test.assertEquals(code(0), control(0));
-//             });
-//             it ('returns the square of 2', function(){
-//                 Test.assertEquals(code(2), control(2));
-//             });
-//             it ('returns the square of 9', function(){
-//                 Test.assertEquals(code(9), control(9));
-//             });
-//             it('returns the square of random number', () => {
-//                 const randNum = Math.round(Math.random() * 100)
-//                 Test.assertEquals(code(randNum), control(randNum));
-//             });
-//         });
-//     }
-// }
